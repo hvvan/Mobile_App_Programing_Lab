8ad5a3a6213615179d0a32a96ae8c4a51ca0d23d
hvvan <kimjihwankk@gmail.com>
2023-04-05 00:12:10 +0900

    Uploaded 5th week lab assignment
    I learned constraint layout, inflator. By using this, I made the chatroom list !

---
+++ Recap_Week5/app/src/main/java/com/example/recap_week5/Chatroom.kt
@@ -0,0 +1,11 @@
+package com.example.recap_week5
+
+class Chatroom(
+    val name: String,
+    val lastChat: String,
+    val thumbnail: Int,
+    val groupNumber: Int,
+    val lastTime: String,
+    val unreadmsg:Int
+) {
+}
